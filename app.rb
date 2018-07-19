245a97e12e869edf8a5f6e6f7ad0b9adbbfae8a6
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-19 00:37:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,4 +3,18 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/reversename/:name' do
+    
+  end
+
+  get '/square/:number' do
+    
+  end
+
+  get '/say/:number/:phrase' do
+    
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5'
+
 end
