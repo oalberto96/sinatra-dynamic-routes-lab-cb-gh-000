6378cefb3ebb41e4fe287a2d7292105a50b8592b
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-19 00:38:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ class App < Sinatra::Base
   # Write your code here!
 
   get '/reversename/:name' do
-
+    params[:name].reverse
   end
 
   get '/square/:number' do
@@ -16,11 +16,11 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    
+
   end
 
   get '/:operation/:number1/:number2' do
-    
+
   end
 
 end
